642b5fc475e7cb2fce5a9e49cbaab2b392d31fb7
Sheldon Tower <towersheldon@gmail.com>
2022-09-22 21:37:26 -0230

    React component for requesting information on ham station callsigns from hamQTH.com

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { useState } from "react";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+function Location(){
+
+  const [callSign, setCallSign] = useState(0);
+  let obj;
+ 
+    fetch('https://www.hamqth.com/dxcc.php?callsign=' + callSign, {mode: 'no-cors'})
+    .then((response) => {response.text()})
+    .then((text) => {console.log(text)})
+    
+  return(
+    <div>
+      <label htmlFor="callSign" >Callsign: </label> <input type="text" id="callSign" name="callSign" />
+      <button onClick={() => setCallSign(document.getElementById("callSign").value)} > Submit </button>
+    </div>
+  );
+
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  
+      <Location />
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+);
+//<React.StrictMode></React.StrictMode>
