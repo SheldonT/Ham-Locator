f56456676440636879c58dccb6d946b0f4177e0e
Sheldon Tower <towersheldon@gmail.com>
2022-09-24 23:26:03 -0230

    Troubleshooting CORS error with gh-pages

--- src/useFetch.js
+++ src/useFetch.js
@@ -4,7 +4,7 @@ export default function useFetch(call){
     const [data, setData] = useState(null);
 
     useEffect (() => {
-        const url = "/dxcc_json.php?callsign=" + call;
+        const url = "https://www.hamqth.com/dxcc_json.php?callsign=" + call;
         if (call !== 0) fetch(url).then((res) => res.json()).then((d) => setData(d)).catch((e) => console.log(e));
     }, [call]);
 
