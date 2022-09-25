9c82f4aa2733f4c591e02f33abf7c483754216e4
Sheldon Tower <towersheldon@gmail.com>
2022-09-24 22:39:12 -0230

    Added a table to display the country name, and latitude and longitude coordinates of the station callsign Added a world map to plot the location of the station

--- src/index.js
+++ src/index.js
@@ -1,31 +1,83 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { useState } from "react";
+import {useState} from "react";
+import {Map, Marker} from "pigeon-maps"
+import useFetch from "./useFetch.js";
 import './index.css';
 
+function CallMap(props){
+
+  console.log(props.coord);
+
+  return(
+    <div className="map">
+      <Map height={600} defaultCenter={[0, 0]} defaultZoom={3}>
+        <Marker width={40} anchor={props.coord} />
+      </Map>
+    </div>
+  );
+}
+
+function InfoBar(props){
+
+  return(
+    <div className="infoBar">
+      <table>
+        <tbody>
+          <tr className="tableColumn" >
+            <th> Call Sign </th>
+            <th> Country </th>
+            <th> Latitude </th>
+            <th> Longitude </th>
+          </tr>
+          <tr >
+            <td>{props.callsign}</td>
+            <td>{props.country}</td>
+            <td>{props.lat}</td>
+            <td>{props.lng}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
 
 function Location(){
 
   const [callSign, setCallSign] = useState(0);
-  let obj;
- 
-    fetch('https://www.hamqth.com/dxcc.php?callsign=' + callSign, {mode: 'no-cors'})
-    .then((response) => {response.text()})
-    .then((text) => {console.log(text)})
-    
+
+  const jsonResp = useFetch(callSign);
+  let rCall = "";
+  let rCountry = "";
+  let rLatitude = "";
+  let rLongitude = "";
+  let anchor=[0, 0];
+
+  if (jsonResp[0]){
+    rCall = jsonResp[0].callsign;
+    rCountry = jsonResp[0].name;
+    rLatitude = jsonResp[0].lat;
+    rLongitude = jsonResp[0].lng;
+    anchor=[parseFloat(rLatitude), parseFloat(rLongitude)];
+  }
+
   return(
-    <div>
-      <label htmlFor="callSign" >Callsign: </label> <input type="text" id="callSign" name="callSign" />
-      <button onClick={() => setCallSign(document.getElementById("callSign").value)} > Submit </button>
-    </div>
+    <>
+      <CallMap coord={anchor} />
+      <InfoBar callsign={rCall} country={rCountry} lat={rLatitude} lng={rLongitude} />
+      <div className="bottomBar">
+        <label htmlFor="callSign" >Callsign:</label> <input className="callField" type="text" id="callSign" name="callSign" />
+        <button className="submit" onClick={() => setCallSign(document.getElementById("callSign").value)} > Submit </button>
+      </div>
+    </>
   );
 
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
-      <Location />
+  <React.StrictMode>
+    <Location />
+  </React.StrictMode>
 
 );
-//<React.StrictMode></React.StrictMode>
---
+++ src/useFetch.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+export default function useFetch(call){
+    const [data, setData] = useState(null);
+
+    useEffect (() => {
+        const url = "http://localhost:3000/dxcc_json.php?callsign=" + call;
+        if (call !== 0) fetch(url).then((res) => res.json()).then((d) => setData(d)).catch((e) => console.log(e));
+    }, [call]);
+
+    return [data];
+
+}
