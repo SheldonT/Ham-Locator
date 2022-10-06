636a704941cdc5a7aaae7423182dd6840909b06d
Sheldon Tower <towersheldon@gmail.com>
2022-10-05 22:55:00 -0230

    Created hover affect for table rows.

--- src/index.js
+++ src/index.js
@@ -1,70 +1,92 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import ReactDOM from 'react-dom/client';
-import {useState} from "react";
-import {Map, Marker} from "pigeon-maps"
+import {Map, Marker, Overlay} from "pigeon-maps";
 import useFetch from "./useFetch.js";
 import './index.css';
 
-function CallMap(props){
-
-  console.log(props.coord);
+function CallMap({coord, selectedId}){
 
   return(
     <div className="map">
       <Map height={600} defaultCenter={[0, 0]} defaultZoom={3}>
-        <Marker width={40} anchor={props.coord} />
+        {coord.map((mapCoord) => 
+          <Marker width={40} color={selectedId === mapCoord[2] ? "hsl(210deg 100% 50%)" : "hsl(280deg 100% 50%)" } anchor={mapCoord} id={selectedId} >
+            <span>Test</span>
+          </ Marker>
+        )}
       </Map>
     </div>
   );
 }
 
-function InfoBar(props){
+//<Marker width={40} color={selectedId === mapCoord[2] ? "hsl(210deg 100% 50%)" : "hsl(280deg 100% 50%)" } anchor={mapCoord} id={selectedId} />
 
+
+function TableRow({info, action}){
+  
   return(
-    <div className="infoBar">
-      <table>
-        <tbody>
-          <tr className="tableColumn" >
-            <th> Call Sign </th>
-            <th> Country </th>
-            <th> Latitude </th>
-            <th> Longitude </th>
-          </tr>
-          <tr >
-            <td>{props.callsign}</td>
-            <td>{props.country}</td>
-            <td>{props.lat}</td>
-            <td>{props.lng}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <>
+        <tr onMouseEnter={() => action(info.id) } onMouseLeave={ () => action() } id={info.id}>
+          <td>{info.anchor?.[2]}</td>
+          <td>{info.call}</td>
+          <td>{info.country}</td>
+          <td>{info.anchor?.[0]}</td>
+          <td>{info.anchor?.[1]}</td>
+        </tr>
+    </>
   );
 }
 
-function Location(){
+//onMouseEnter={() => console.log(props.info.anchor?.[2])} onMouseLeave={() => console.log("Mouse Leave")}
 
-  const [callSign, setCallSign] = useState(0);
-
-  const jsonResp = useFetch(callSign);
-  let rCall = "";
-  let rCountry = "";
-  let rLatitude = "";
-  let rLongitude = "";
-  let anchor=[0, 0];
-
-  if (jsonResp[0]){
-    rCall = jsonResp[0].callsign;
-    rCountry = jsonResp[0].name;
-    rLatitude = jsonResp[0].lat;
-    rLongitude = jsonResp[0].lng;
-    anchor=[parseFloat(rLatitude), parseFloat(rLongitude)];
-  }
+function InfoBar({info, action}){
 
   return(
     <>
-      <CallMap coord={anchor} />
-      <InfoBar callsign={rCall} country={rCountry} lat={rLatitude} lng={rLongitude} />
+    <div className="infoBar">
+      <table id="callList">
+        <thead>
+        <tr>
+          <th> # </th>
+          <th> Call Sign </th>
+          <th> Country </th>
+          <th> Latitude </th>
+          <th> Longitude </th>
+        </tr>
+        </thead>
+        <tbody>
+        {info.map( (callData) => <TableRow info={callData} action={action} />)}
+      </tbody>
+    </table>
+  </div>
+  </>
+  );
+}
+
+
+function Location(){
+
+  const [callSign, setCallSign] = useState("");
+  const [anchorList, setAnchorList] = useState([]);
+  const [infoList, setInfoList] = useState([]);
+  const [activeRow, setActiveRow] = useState();
+
+  const jsonResp = useFetch(callSign);
+
+  useEffect( () => {
+    if((jsonResp.anchor) && (callSign !== "")) {
+      setAnchorList((previousAnchors) => [...previousAnchors, jsonResp.anchor]);
+      setInfoList( (previousInfo) => [...previousInfo, Object.assign({call: callSign}, jsonResp)]);
+    }
+
+  }, [jsonResp]);
+
+console.log(activeRow);
+
+  return(
+    <>
+      <CallMap coord={anchorList} selectedId={activeRow} />
+      <InfoBar info={infoList} action={setActiveRow}/>
       <div className="bottomBar">
         <label htmlFor="callSign" >Callsign:</label> <input className="callField" type="text" id="callSign" name="callSign" />
         <button className="submit" onClick={() => setCallSign(document.getElementById("callSign").value)} > Submit </button>
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,13 +1,35 @@
 import { useState, useEffect } from "react";
 
+let i = 0;
+
 export default function useFetch(call){
     const [data, setData] = useState(null);
+    //const defaultColor = "hsl(210deg 100% 50%)";
+    
 
     useEffect (() => {
-        const url = "https://www.hamqth.com/dxcc_json.php?callsign=" + call;
-        if (call !== 0) fetch(url).then((res) => res.json()).then((d) => setData(d)).catch((e) => console.log(e));
+        const url = "/dxcc_json.php?callsign=" + call;
+
+        if (call !== ""){
+             
+            i = i + 1;
+
+            fetch(url).then((res) => res.json()).then((d) => {
+                let rCountry = d.name;
+                let rLatitude = d.lat;
+                let rLongitude = d.lng;
+            
+                setData({
+                    anchor: [parseFloat(rLatitude), parseFloat(rLongitude), i],
+                    id: i,
+                    country: rCountry,
+                });
+        
+            }).catch((e) => console.log(e));
+        }
+
     }, [call]);
 
-    return [data];
+    return data || {}; //if data is null, return an empty string.
 
 }
