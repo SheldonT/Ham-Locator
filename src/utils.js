30e4ff8f453e85dcc62aac51f1aee0be595c874b
Sheldon Tower <towersheldon@gmail.com>
2023-04-28 13:43:38 -0230

    Updated backend api url

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,7 @@
 export const getStageURL = (window) => {
   const location = window.location.hostname;
   const apiHost =
-    "http://ham-locator-server-4-env.eba-t7ewg8qf.ca-central-1.elasticbeanstalk.com";
+    "http://ham-locator-server-2-112-env.eba-yrqebr4m.ca-central-1.elasticbeanstalk.com/";
 
   if (location.includes("localhost")) {
     return "http://localhost:3007";
