f5078a8af7666e7e4239d67e07c56623612ad166
Sheldon Tower <towersheldon@gmail.com>
2023-05-23 16:43:50 -0230

    removed trailing / from api url

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,7 @@
 export const getStageURL = (window) => {
   const location = window.location.hostname;
   const apiHost =
-    "https://ham-locator-svr-https-env.eba-whsc5wsc.ca-central-1.elasticbeanstalk.com/";
+    "https://ham-locator-svr-https-env.eba-whsc5wsc.ca-central-1.elasticbeanstalk.com";
 
   if (location.includes("localhost")) {
     return "http://localhost:3007";
