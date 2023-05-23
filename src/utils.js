943e51c6752460d409ce9c27022a7691b56a6662
Sheldon Tower <towersheldon@gmail.com>
2023-05-23 13:34:23 -0230

    Merge branch 'main' of https://github.com/SheldonT/Ham-Locator

--- src/utils.js
+++ src/utils.js
@@ -2,8 +2,7 @@
 
 export const getStageURL = (window) => {
   const location = window.location.hostname;
-  const apiHost =
-    "http://ham-locator-server-2-112-env.eba-yrqebr4m.ca-central-1.elasticbeanstalk.com";
+  const apiHost = "https://hamlocator.space";
 
   if (location.includes("localhost")) {
     return "http://localhost:3007";
