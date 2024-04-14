927b8b918639f70747359500d40bec9d960831b1
SheldonT <109766064+SheldonT@users.noreply.github.com>
2024-04-14 12:18:44 -0230

    Migrating to fly.io backend

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,8 @@
 
 export const getStageURL = (window) => {
   const location = window.location.hostname;
-  const apiHost = "https://hamlocator.space";
+  //const apiHost = "https://hamlocator.space";
+  const apiHost = "https://ham-locator-server.fly.dev";
 
   if (location.includes("localhost")) {
     return "http://localhost:3007";
