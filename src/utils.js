dd93a5b1e9aff061bd4600bad54ee2b735ae11a8
SheldonT <109766064+SheldonT@users.noreply.github.com>
2024-07-06 19:57:08 -0230

    changed backend host

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,7 @@
 export const getStageURL = (window) => {
   const location = window.location.hostname;
   //const apiHost = "https://hamlocator.space";
-  const apiHost = "https://ham-locator-server-postgresql.fly.dev";
+  const apiHost = "https://hamlocator.space:3007";
 
   if (location.includes("localhost")) {
     return "http://localhost:3007";
