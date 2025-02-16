5330108b2c08a7c2381c50e7cd7e6a1fa3a5a7aa
SheldonT <109766064+SheldonT@users.noreply.github.com>
2025-02-16 17:42:54 -0330

    Changed Backend URL

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,8 @@
 export const getStageURL = (window) => {
   const location = window.location.hostname;
   //const apiHost = "https://hamlocator.space";
-  const apiHost = "https://hamlocator.space:3007";
+  const apiHost =
+    " https://ham-locator-server-383972071360.northamerica-northeast1.run.app";
 
   if (location.includes("localhost")) {
     return "http://localhost:3007";
