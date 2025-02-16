6abe3c559c3ba4ef9df5e2d4a0fc8ae7a499915d
SheldonT <109766064+SheldonT@users.noreply.github.com>
2025-02-16 17:48:13 -0330

    Corrected Backend Host URL

--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,7 @@ export const getStageURL = (window) => {
   const location = window.location.hostname;
   //const apiHost = "https://hamlocator.space";
   const apiHost =
-    " https://ham-locator-server-383972071360.northamerica-northeast1.run.app";
+    "https://ham-locator-server-383972071360.northamerica-northeast1.run.app";
 
   if (location.includes("localhost")) {
     return "http://localhost:3007";
